6d639e19521d78fb252dee3fd0cb743cf957c509
Vikramaditya Singh <vikramadityascode@gmail.com>
2024-08-12 21:50:02 +0530

    mark in devlopemnt

--- blog-frontend/src/app/page.tsx
+++ blog-frontend/src/app/page.tsx
@@ -1,12 +1,15 @@
 import LatestPost from "@/components/atom/latestPost";
 import BackGroundParticle from "@/components/atom/backGroundParticle";
 import FeaturedPost from "@/components/atom/featuredPost";
+import InDevelopment from "@/components/molecules/inDevelopment";
+import React from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <LatestPost />
-      <FeaturedPost />
+      <main className="flex flex-col items-center justify-between pt-32 px-12 md:px-24 lg:px-32 xl:px-48">
+        <InDevelopment />
+      </main>
       <BackGroundParticle />
     </main>
   );
