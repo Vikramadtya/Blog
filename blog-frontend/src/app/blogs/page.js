c2e7dc340d144e62300b70921d8a37b854a53a01
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2024-09-13 23:22:01 +0530

    init

--- blog-frontend/src/app/blogs/page.js
+++ blog-frontend/src/app/blogs/page.js
@@ -6,9 +6,10 @@ import matter from "gray-matter";
 
 import MarkDownContentList from "../../components/molecules/markDownContentList";
 import { getBlogsMetaDataFromRemote } from "../../services/blogService";
+import { getAllBlogsMetaData, getLikes } from "../../services/apiServices";
 
 export default async function Blog() {
-  const blogs = await getBlogsMetaDataFromRemote();
+  const blogs = await getAllBlogsMetaData();
 
   return (
     <main className="flex flex-col items-center justify-between px-12 md:px-24 lg:px-32 xl:px-48">
