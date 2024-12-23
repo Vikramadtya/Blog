d7742d1184ce3628ef052fdcc81af35469d4bdb2
Vikramaditya Singh <86847191+Vikramadtya@users.noreply.github.com>
2024-12-23 14:28:05 +0530

    Update latestPost.jsx

--- blog-frontend/src/components/molecules/latestPost.jsx
+++ blog-frontend/src/components/molecules/latestPost.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Tag from "@/components/atom/tag";
+import Link from "next/link";
 
 const LatestPost = ({ title, description, tags, slug, previewImageSrc }) => {
   const tagsComponent = tags.map((tag) => (
@@ -16,17 +17,17 @@ const LatestPost = ({ title, description, tags, slug, previewImageSrc }) => {
             </div>
           </div>
           <div className="flex flex-col gap-4">
-            <a href={`/blogs/${slug}`}>
+            <Link href={`/blogs/${slug}`}>
               <h1 className="text-2xl font-bold leading-[1.1] tracking-wider md:text-[40px]">
                 {title}
               </h1>
-            </a>
+            </Link>
 
             <p className="text-muted-foreground">{description}</p>
             <div className="flex flex-wrap gap-2"></div>
           </div>
           <div className="flex flex-wrap gap-1">{...tagsComponent}</div>
-          <a
+          <Link
             href={`/blogs/${slug}`}
             className="mb-2 me-2 mt-2 inline-flex w-40 items-center rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
           >
@@ -46,7 +47,7 @@ const LatestPost = ({ title, description, tags, slug, previewImageSrc }) => {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
+          </Link>
         </div>
         <div className="flex hidden justify-center md:block">
           <div className="p-2 pt-5">
